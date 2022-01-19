ffd57969c154ce382b32dd06101a594e705c43ca
Sakazaki <sakatomo.z0207@gmail.com>
2022-01-19 16:17:44 +0900

    コントローラーのBボタンで一時停止できるようになり、一時停止の文字が表示されるようになった

--- Assets/Script/Pause.cs
+++ Assets/Script/Pause.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Pause : MonoBehaviour
 {
+    public string paose;
+
     [SerializeField]
     private GameObject pauseUIPrefab;
 
     private GameObject pauseUIInstance;
+
+    [SerializeField] GameObject Paose;
 
     // Start is called before the first frame update
     void Start()
@@ -18,17 +22,19 @@ public class Pause : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("q"))
+        if (Input.GetKeyDown("joystick button 1"))
         {
             if(pauseUIInstance == null)
             {
                 pauseUIInstance = GameObject.Instantiate(pauseUIPrefab) as GameObject;
                 Time.timeScale = 0f;
+                Paose.SetActive(true);
             }
             else
             {
                 Destroy(pauseUIInstance);
                 Time.timeScale = 1f;
+                Paose.SetActive(false);
             }
         }
 

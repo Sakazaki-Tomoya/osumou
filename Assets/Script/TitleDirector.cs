867ac146321c9376c6b0f315d6a968f4775af31c
Sakazaki <sakatomo.z0207@gmail.com>
2022-01-24 13:48:13 +0900

    0124sakazaki最新

---
+++ Assets/Script/TitleDirector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleDirector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        //Aを押したときにGameSceneに変わる
+        if(Input.GetKeyDown("joystick button 0"))
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+        //if (Input.GetKeyDown("Mouce Botton 0"))
+        //{
+        //    SceneManager.LoadScene("GameScene");
+        //}
+    }
+}
